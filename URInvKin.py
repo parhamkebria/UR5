730cfbd9da129c9c01b4fe82c7d9f1c330650c7d
Parham Kebria <parham.m.kebria@gmail.com>
2026-01-24 21:56:15 -0500

    UR forward and inverse kinematics

--- URInvKin.py
+++ URInvKin.py
@@ -49,7 +49,7 @@ elif UR5:
 # FORWARD KINEMATICS
 # ============================================================================
 
-def AH(n, th, c):
+def compute_dh_transform(n, th, c):
     """
     Compute the Denavit-Hartenberg transformation matrix for joint n.
     
@@ -89,7 +89,7 @@ def AH(n, th, c):
     return A_i
 
 
-def HTrans(th, c):
+def forward_kinematics(th, c):
     """
     Compute the homogeneous transformation matrix from base to end-effector.
     
@@ -100,12 +100,12 @@ def HTrans(th, c):
     Returns:
         4x4 homogeneous transformation matrix from base to end-effector
     """
-    A_1 = AH(1, th, c)
-    A_2 = AH(2, th, c)
-    A_3 = AH(3, th, c)
-    A_4 = AH(4, th, c)
-    A_5 = AH(5, th, c)
-    A_6 = AH(6, th, c)
+    A_1 = compute_dh_transform(1, th, c)
+    A_2 = compute_dh_transform(2, th, c)
+    A_3 = compute_dh_transform(3, th, c)
+    A_4 = compute_dh_transform(4, th, c)
+    A_5 = compute_dh_transform(5, th, c)
+    A_6 = compute_dh_transform(6, th, c)
         
     T_06 = A_1 * A_2 * A_3 * A_4 * A_5 * A_6
 
@@ -146,7 +146,7 @@ def invKine(desired_pos):
     cl = [0, 4]
     for i in range(len(cl)):
         c = cl[i]
-        T_10 = linalg.inv(AH(1, th, c))
+        T_10 = linalg.inv(compute_dh_transform(1, th, c))
         T_16 = T_10 * desired_pos
         th[4, c:c+2] = +acos((T_16[2, 3] - d4) / d6)
         th[4, c+2:c+4] = -acos((T_16[2, 3] - d4) / d6)
@@ -158,7 +158,7 @@ def invKine(desired_pos):
     cl = [0, 2, 4, 6]
     for i in range(len(cl)):
         c = cl[i]
-        T_10 = linalg.inv(AH(1, th, c))
+        T_10 = linalg.inv(compute_dh_transform(1, th, c))
         T_16 = linalg.inv(T_10 * desired_pos)
         th[5, c:c+2] = atan2((-T_16[1, 2] / sin(th[4, c])),
                             (T_16[0, 2] / sin(th[4, c])))
@@ -169,9 +169,9 @@ def invKine(desired_pos):
     cl = [0, 2, 4, 6]
     for i in range(len(cl)):
         c = cl[i]
-        T_10 = linalg.inv(AH(1, th, c))
-        T_65 = AH(6, th, c)
-        T_54 = AH(5, th, c)
+        T_10 = linalg.inv(compute_dh_transform(1, th, c))
+        T_65 = compute_dh_transform(6, th, c)
+        T_54 = compute_dh_transform(5, th, c)
         T_14 = (T_10 * desired_pos) * linalg.inv(T_54 * T_65)
         P_13 = T_14 * MATRIX([0, -d4, 0, 1]).T - MATRIX([0, 0, 0, 1]).T
         t3 = cmath.acos((linalg.norm(P_13)**2 - a2**2 - a3**2) / (2 * a2 * a3))
@@ -182,9 +182,9 @@ def invKine(desired_pos):
     cl = [0, 1, 2, 3, 4, 5, 6, 7]
     for i in range(len(cl)):
         c = cl[i]
-        T_10 = linalg.inv(AH(1, th, c))
-        T_65 = linalg.inv(AH(6, th, c))
-        T_54 = linalg.inv(AH(5, th, c))
+        T_10 = linalg.inv(compute_dh_transform(1, th, c))
+        T_65 = linalg.inv(compute_dh_transform(6, th, c))
+        T_54 = linalg.inv(compute_dh_transform(5, th, c))
         T_14 = (T_10 * desired_pos) * T_65 * T_54
         P_13 = T_14 * MATRIX([0, -d4, 0, 1]).T - MATRIX([0, 0, 0, 1]).T
         
@@ -192,8 +192,8 @@ def invKine(desired_pos):
         th[1, c] = -atan2(P_13[1], -P_13[0]) + asin(a3 * sin(th[2, c]) / linalg.norm(P_13))
         
         # Joint 4 (wrist 1)
-        T_32 = linalg.inv(AH(3, th, c))
-        T_21 = linalg.inv(AH(2, th, c))
+        T_32 = linalg.inv(compute_dh_transform(3, th, c))
+        T_21 = linalg.inv(compute_dh_transform(2, th, c))
         T_34 = T_32 * T_21 * T_14
         th[3, c] = atan2(T_34[1, 0], T_34[0, 0])
     
